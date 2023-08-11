8bcc3d66226a58f1ed9017127c94359b44c7c80f
Diogo Merelles <diogo.merelles@xing.com>
2023-08-11 12:09:39 +0100

    added better api call to fetch random movies

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,12 +4,19 @@ const BASE_URL = 'https://api.themoviedb.org/3';
 const API_KEY = 'af2fff72d2853697571a81a37b1bd0fc'; 
 
 export const fetchRandomMovie = async () => {
-  // For simplicity, I'm fetching popular movies. You can adjust this to make it more random.
-  const response = await axios.get(`${BASE_URL}/movie/popular?api_key=${API_KEY}&language=en-US&page=1`);
-  const movies = response.data.results;
-  return movies[Math.floor(Math.random() * movies.length)];
-};
-
+    const totalPages = 10; // Number of pages to fetch
+  
+    const allMovies = [];
+  
+    for (let page = 1; page <= totalPages; page++) {
+      const response = await axios.get(`${BASE_URL}/movie/top_rated?api_key=${API_KEY}&language=en-US&page=${page}`);
+      const movies = response.data.results;
+      allMovies.push(...movies);
+    }
+      const randomIndex = Math.floor(Math.random() * allMovies.length);
+    return allMovies[randomIndex];
+  };
+  
 export const searchMovies = async (query: string) => {
     try {
       const response = await axios.get(`${BASE_URL}/search/movie`, {
--- src/views/MovieWall.tsx
+++ src/views/MovieWall.tsx
@@ -2,29 +2,49 @@ import React, { useEffect, useState } from 'react';
 import { useMovies } from '../context/MovieContext';
 import { fetchRandomMovie } from '../services/api';
 import { Movie } from '../types';
-import MovieCard from '../components/MovieCard'; // Assuming you have this component
+import MovieCard from '../components/MovieCard';
 
 const MovieWall: React.FC = () => {
   const [movie, setMovie] = useState<Movie | null>(null);
   const { addFavorite, addToWallOfShame } = useMovies();
 
-  useEffect(() => {
-    const getRandomMovie = async () => {
+  const getRandomMovie = async () => {
+    try {
       const data = await fetchRandomMovie();
       setMovie(data);
-    };
+    } catch (error) {
+      console.error("Failed to fetch a random movie:", error);
+    }
+  };
+
+  useEffect(() => {
     getRandomMovie();
   }, []);
+
+  const handleLike = () => {
+    if (movie) {
+      addFavorite(movie);
+      getRandomMovie();  // Fetch a new random movie
+    }
+  };
+
+  const handleDislike = () => {
+    if (movie) {
+      addToWallOfShame(movie);
+      getRandomMovie();  // Fetch a new random movie
+    }
+  };
 
   if (!movie) return <div>Loading...</div>;
 
   return (
     <div>
       <MovieCard movie={movie} />
-      <button onClick={() => addFavorite(movie)}>Like</button>
-      <button onClick={() => addToWallOfShame(movie)}>Dislike</button>
+      <button onClick={handleLike}>Like</button>
+      <button onClick={handleDislike}>Dislike</button>
     </div>
   );
 };
 
 export default MovieWall;
+
